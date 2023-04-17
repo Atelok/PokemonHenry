b283d9b0a512f0ca0d6360c994593a15dce6a336
Atelok <diego135tc@gmail.com>
2023-04-17 03:36:12 -0300

    Lunes 17/04/2023 - 03:36

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -3,20 +3,26 @@ import style from "./Card.module.css"
 const Card = ({ id, name, image, Types }) => {
     return (
         <div className={style.div_general_card}>
+
+            {/* IMAGEN DE LA CARD */}
             <div className={style.div_img_pokemon}>
                 <img className={style.img_pokemon} src={image} alt={`Pokemons ${name}`} />
             </div>
+
+            {/* #ID O #CREATED */}
             {isNaN(id)? (<div># Created </div>):(<div> # {id} </div>)}
 
+            {/* NOMBRE DEL POKE */}
             <div >
-            {/* <h4> Name : </h4> */}
             <h4>{name}</h4>
             </div>
             
+            {/* H4 DE TYPES */}
             <div className={style.nameCard}>
             <h4>Types:</h4>
             </div>
             
+            {/* LOS TIPOS DE POKEMON */}
             <div className={style.div_types}>
                 {/* AQUI VA LA VERIFICACION DE types y Types */}
                 {typeof Types[0] === "string" ? (
@@ -26,9 +32,9 @@ const Card = ({ id, name, image, Types }) => {
                     </>
                 ) : ("")
                 }
-
-
             </div>
+
+
         </div>
     )
 }
