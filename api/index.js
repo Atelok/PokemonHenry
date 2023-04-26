abb91909471ba9589b7508b1d8d8f2e66aa9a807
Atelok <diego135tc@gmail.com>
2023-04-26 11:51:43 -0300

    Ready

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 // "alter" va a modificar y sincronizar sin tirarse la barra entera force
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log('Server is listening at 3001'); // eslint-disable-line no-console
   });
