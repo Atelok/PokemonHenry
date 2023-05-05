dcbc1d47ad07d239107075489710f25ed8c362ea
Atelok <diego135tc@gmail.com>
2023-05-04 21:42:33 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require("dotenv").config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT   || 3001
 
 // Syncing all the models at once.
 // "alter" va a modificar y sincronizar sin tirarse la barra entera force
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+conn.sync({ force: false }).then(() => {
+  server.listen(port, () => {
     console.log('Server is listening at 3001'); // eslint-disable-line no-console
   });
 });
